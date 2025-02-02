59cbd33245fe38d0f5caaa1067f2da5737ac11a6
linfeng-du <linfeng9918@gmail.com>
2025-02-01 20:28:26 -0500

    add BanditPR trianing

---
+++ src/train.py
@@ -0,0 +1,38 @@
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+import torch
+
+from rl import ProfileScoreModel
+from trainer import RetrieverTrainer
+
+
+@hydra.main(version_base=None, config_path='../conf', config_name='train_config')
+def train(config: DictConfig):
+    # Check config validity
+    missing_keys = OmegaConf.missing_keys(config)
+
+    if missing_keys:
+        raise ValueError(f'Missing keys in config:\n{missing_keys}')
+
+    if config.eval_every % config.batch_size != 0:
+        raise ValueError(f'eval_every must be divisble by batch_size')
+
+    # Seed everything for reproducibility
+    torch.manual_seed(config.seed)
+    torch.cuda.manual_seed_all(config.seed)
+
+    # Detect device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Prepare score model for retriever training
+    score_model = ProfileScoreModel(**config.score_model)
+    score_model.to(device)
+
+    # Initialize trainer and start training
+    trainer = RetrieverTrainer(config, score_model, device)
+    trainer.train()
+
+
+if __name__ == '__main__':
+    train()
