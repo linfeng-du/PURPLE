f34a662c7c31e8fafdf1649be15bd13f9c0a6ea3
linfeng-du <linfeng9918@gmail.com>
2025-02-20 16:52:12 -0500

    update api call

--- src/openai_api.py
+++ src/openai_api.py
@@ -1,7 +1,11 @@
 import os
+import logging
 from typing import Callable
 
-from openai import OpenAI
+from openai import OpenAI, InternalServerError
+
+
+logger = logging.getLogger(__name__)
 
 
 def initialize_openai_client(model: str, temperature: float) -> (
@@ -19,16 +23,23 @@ def initialize_openai_client(model: str, temperature: float) -> (
     client = OpenAI(api_key=api_key, base_url=base_url)
 
     def request_completions(prompts: list[str]) -> list[str]:
-        responses = []
+        responses = [None for _ in range(len(prompts))]
+        remaining_prompts = set(range(len(prompts)))
 
-        for prompt in prompts:
-            completion = client.chat.completions.create(
-                model=model,
-                messages=[{'role': 'user', 'content': prompt}],
-                temperature=temperature
-            )
-            response = completion.choices[0].message.content
-            responses.append(response)
+        while remaining_prompts:
+            for prompt_index in list(remaining_prompts):
+                try:
+                    completion = client.chat.completions.create(
+                        model=model,
+                        messages=[{'role': 'user', 'content': prompts[prompt_index]}],
+                        temperature=temperature
+                    )
+                    response = completion.choices[0].message.content
+
+                    responses[prompt_index] = response
+                    remaining_prompts.remove(prompt_index)
+                except InternalServerError:
+                    logger.warning(f'Retrying due to server error...')
 
         return responses
 
