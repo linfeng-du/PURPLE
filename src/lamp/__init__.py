a7f2d663350b3d3030bfebaa1db4f6a8b8387a10
linfeng.du <linfeng9918@gmail.com>
2025-04-30 23:25:29 -0700

    update lamp

--- src/lamp/__init__.py
+++ src/lamp/__init__.py
@@ -1,3 +1,3 @@
-from .metrics import create_metric
+from .metrics import create_metric, get_labels
 from .prompts import create_prompt_generator
 from .datasets import LaMPDataset, RetrieverTrainingDataset, RetrieverTrainingCollator
