5da56b62cf44dd5cf3c94efa1216dbd67fd0bcdf
linfeng-du <linfeng9918@gmail.com>
2025-11-14 16:32:21 -0500

    add download script

---
+++ scripts/download.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+mkdir -p data/LaMP-1
+wget -P data/LaMP-1 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_1/train/train_questions.json
+wget -P data/LaMP-1 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_1/train/train_outputs.json
+wget -P data/LaMP-1 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_1/dev/dev_questions.json
+wget -P data/LaMP-1 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_1/dev/dev_outputs.json
+
+mkdir -p data/LaMP-2
+wget -P data/LaMP-2 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_2/new/train/train_questions.json
+wget -P data/LaMP-2 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_2/new/train/train_outputs.json
+wget -P data/LaMP-2 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_2/new/dev/dev_questions.json
+wget -P data/LaMP-2 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_2/new/dev/dev_outputs.json
+
+mkdir -p data/LaMP-3
+wget -P data/LaMP-3 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_3/train/train_questions.json
+wget -P data/LaMP-3 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_3/train/train_outputs.json
+wget -P data/LaMP-3 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_3/dev/dev_questions.json
+wget -P data/LaMP-3 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_3/dev/dev_outputs.json
+
+mkdir -p data/LaMP-4
+wget -P data/LaMP-4 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_4/train/train_questions.json
+wget -P data/LaMP-4 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_4/train/train_outputs.json
+wget -P data/LaMP-4 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_4/dev/dev_questions.json
+wget -P data/LaMP-4 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_4/dev/dev_outputs.json
+
+mkdir -p data/LaMP-5
+wget -P data/LaMP-5 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_5/train/train_questions.json
+wget -P data/LaMP-5 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_5/train/train_outputs.json
+wget -P data/LaMP-5 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_5/dev/dev_questions.json
+wget -P data/LaMP-5 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_5/dev/dev_outputs.json
+
+mkdir -p data/LaMP-7
+wget -P data/LaMP-7 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_7/train/train_questions.json
+wget -P data/LaMP-7 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_7/train/train_outputs.json
+wget -P data/LaMP-7 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_7/dev/dev_questions.json
+wget -P data/LaMP-7 https://ciir.cs.umass.edu/downloads/LaMP/LaMP_7/dev/dev_outputs.json
+
+python - << 'EOF'
+from datasets import load_dataset
+
+load_dataset("LongLaMP/LongLaMP", name="abstract_generation_user")
+load_dataset("LongLaMP/LongLaMP", name="topic_writing_user")
+load_dataset("LongLaMP/LongLaMP", name="product_review_user")
+EOF
+
+hf download facebook/contriever
+hf download microsoft/Phi-4-mini-instruct
+hf download meta-llama/Meta-Llama-3-8B-Instruct
+hf download meta-llama/Meta-Llama-3-70B-Instruct
+
+python - << 'EOF'
+from evaluate import load
+
+load("accuracy")
+load("f1")
+load("mae")
+load("mse")
+load("rouge")
+load("meteor")
+EOF
