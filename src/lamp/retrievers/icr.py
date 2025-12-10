f31645d61c887ec6ce0189a66f4114a12fb78e5e
linfeng-du <linfeng9918@gmail.com>
2025-12-10 16:38:06 -0500

    update icr

--- src/lamp/retrievers/icr.py
+++ src/lamp/retrievers/icr.py
@@ -1,20 +1,26 @@
-from .in_context_reranker import InContextReranker
-from ..data_types import Profile
+from .in_context_reranker.in_context_reranker import InContextReranker
 
 
 class ICR:
 
-    def __init__(self) -> None:
+    def __init__(self, sliding_window_size: int = 10) -> None:
         self.icr = InContextReranker(
-            base_llm_name='meta-llama/Meta-Llama-3-8B-Instruct',
-            scoring_strategy='masked_NA_calibration',
-            retrieval_type='IE',
-            sliding_window_size=10
+            "meta-llama/Meta-Llama-3-8B-Instruct",
+            scoring_strategy="masked_NA_calibration",
+            retrieval_type="IE",
+            sliding_window_size=sliding_window_size
         )
 
-    def __call__(self, query: str, corpus: list[str], profiles: list[Profile], num_rerank: int) -> (
-        list[Profile]
-    ):
-        corpus = [document.strip() for document in corpus]
+    def __call__(
+        self,
+        query: str,
+        corpus: list[str],
+        profile: list[dict[str, str]],
+        num_retrieve: int
+    ) -> list[dict[str, str]]:
+        assert len(corpus) == len(profile) != 0
+        num_retrieve = min(num_retrieve, len(profile))
+
+        corpus = [doc.strip() for doc in corpus]
         (ranking, _), _ = self.icr.rerank(query, corpus)
-        return [profiles[rank] for rank in ranking[:num_rerank]]
+        return [profile[idx] for idx in ranking[:num_retrieve]]
