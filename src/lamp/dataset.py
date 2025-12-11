2b0eb26610c865aad35c364fe55837b54f25de5d
linfeng-du <linfeng9918@gmail.com>
2025-12-11 10:47:05 -0500

    update dataset

--- src/lamp/dataset.py
+++ src/lamp/dataset.py
@@ -1,198 +1,204 @@
-# Adapted from https://github.com/LaMP-Benchmark/LaMP/blob/main/LaMP/data/datasets.py
-# and https://github.com/LaMP-Benchmark/LaMP/blob/main/LaMP/prompts/prompts.py
+# Adapted from:
+#   https://github.com/LaMP-Benchmark/LaMP/blob/main/LaMP/data/datasets.py
+#   https://github.com/LaMP-Benchmark/LaMP/blob/main/LaMP/prompts/prompts.py
 import json
-import os
+from pathlib import Path
 
 from datasets import Dataset, load_dataset, load_from_disk
 
-from .data_types import Profile
-
 
 def load_lamp_dataset(task: str, split: str) -> Dataset:
-    dataset_dir = f'./dataset/{task}/{split}'
+    dataset_dir = Path("data") / task / split
 
-    if not os.path.exists(dataset_dir):
-        if task.startswith('LaMP'):
-            _process_lamp_dataset(task, split, dataset_dir)
-        elif task.startswith('LongLaMP'):
-            _process_long_lamp_dataset(task, split, dataset_dir)
+    if not dataset_dir.exists():
+        if task.startswith("LaMP"):
+            _prepare_lamp_dataset(task, split, dataset_dir)
+        elif task.startswith("LongLaMP"):
+            _prepare_longlamp_dataset(task, split, dataset_dir)
         else:
-            raise ValueError(f'Invalid task: {task}')
+            raise ValueError(f"Invalid task: {task}")
 
     return load_from_disk(dataset_dir)
 
 
-def _process_lamp_dataset(task: str, split: str, cache_dir: str) -> None:
-    with open(f'./dataset/{task}/{split}_questions.json', 'r') as file:
-        questions = json.load(file)
+def _prepare_lamp_dataset(task: str, split: str, dataset_dir: Path) -> None:
+    questions_file = Path("data") / task / f"{split}_questions.json"
+    outputs_file = Path("data") / task / f"{split}_outputs.json"
 
-    with open(f'./dataset/{task}/{split}_outputs.json', 'r') as file:
-        outputs = {gold['id']: gold['output'] for gold in json.load(file)['golds']}
+    with questions_file.open() as f:
+        questions = json.load(f)
 
-    query_corpus_generator = {
-        'LaMP-1': _generate_query_corpus_classification_citation,
-        'LaMP-2': _generate_query_corpus_classification_movies,
-        'LaMP-3': _generate_query_corpus_regression_review,
-        'LaMP-4': _generate_query_corpus_generation_news,
-        'LaMP-5': _generate_query_corpus_generation_paper,
-        'LaMP-6': _generate_query_corpus_generation_avocado,
-        'LaMP-7': _generate_query_corpus_generation_tweet
-    }[task]
+    with outputs_file.open() as f:
+        outputs = {gold["id"]: gold["output"] for gold in json.load(f)["golds"]}
+
+    query_corpus_generator = QUERY_CORPUS_GENERATORS[task]
     examples = []
 
     for question in questions:
-        source = question['input']
-        profiles = question['profile']
-        target = outputs[question['id']]
-        query, corpus = query_corpus_generator(source, profiles)
+        source = question["input"]
+        profile = question["profile"]
+        target = outputs[question["id"]]
+        query, corpus = query_corpus_generator(source, profile)
 
-        example = {
-            'source': source,
-            'profiles': profiles,
-            'query': query,
-            'corpus': corpus,
-            'target': target
-        }
-        examples.append(example)
+        examples.append({
+            "source": source,
+            "profile": profile,
+            "query": query,
+            "corpus": corpus,
+            "target": target
+        })
 
-    Dataset.from_list(examples).save_to_disk(cache_dir)
+    Dataset.from_list(examples).save_to_disk(dataset_dir)
 
 
-def _process_long_lamp_dataset(task: str, split: str, cache_dir: str) -> None:
-    name = {
-        'LongLaMP-2': 'abstract_generation_user',
-        'LongLaMP-3': 'topic_writing_user',
-        'LongLaMP-4': 'product_review_user'
-    }[task]
-    dataset = load_dataset('LongLaMP/LongLaMP', name=name, split=split)
+LONGLAMP_CONFIGS = {
+    "LongLaMP-2": "abstract_generation_user",
+    "LongLaMP-3": "product_review_user",
+    "LongLaMP-4": "topic_writing_user"
+}
 
-    query_corpus_generator = {
-        'LongLaMP-1': _generate_query_corpus_generation_email,
-        'LongLaMP-2': _generate_query_corpus_generation_abstract,
-        'LongLaMP-3': _generate_query_corpus_generation_topic,
-        'LongLaMP-4': _generate_query_corpus_generation_review
-    }[task]
+
+def _prepare_longlamp_dataset(task: str, split: str, dataset_dir: Path) -> None:
+    dataset = load_dataset("LongLaMP/LongLaMP", name=LONGLAMP_CONFIGS[task], split=split)
+    query_corpus_generator = QUERY_CORPUS_GENERATORS[task]
     examples = []
 
     for row in dataset:
-        source = row['input']
-        profiles = row['profile']
-        target = row['output']
-        query, corpus = query_corpus_generator(source, profiles)
+        source = row["input"]
+        profile = row["profile"]
+        target = row["output"]
+        query, corpus = query_corpus_generator(source, profile)
 
-        example = {
-            'source': source,
-            'profiles': profiles,
-            'query': query,
-            'corpus': corpus,
-            'target': target
-        }
-        examples.append(example)
+        examples.append({
+            "source": source,
+            "profile": profile,
+            "query": query,
+            "corpus": corpus,
+            "target": target
+        })
 
-    Dataset.from_list(examples).save_to_disk(cache_dir)
+    Dataset.from_list(examples).save_to_disk(dataset_dir)
 
 
-# =============================   LaMP 1: Personalized Citation Identification   =============================
-def _generate_query_corpus_classification_citation(source: str, profiles: list[Profile]) -> (
-    tuple[str, list[str]]
-):
+# [LaMP-1] Personalized Citation Identification
+def _generate_query_corpus_classification_citation(
+    source: str,
+    profile: list[dict[str, str]]
+) -> tuple[str, list[str]]:
     reference_1, reference_2 = _extract_references(source)
-    query = f'{reference_1} {reference_2}'
-    corpus = [f'{profile["title"]} {profile["abstract"]}' for profile in profiles]
+    query = f"{reference_1} {reference_2}"
+    corpus = [f"{rec['title']} {rec['abstract']}" for rec in profile]
     return query, corpus
 
 
-# =============================        LaMP 2: Personalized Movie Tagging        =============================
-def _generate_query_corpus_classification_movies(source: str, profiles: list[Profile]) -> (
-    tuple[str, list[str]]
-):
-    query = _extract_string_after_keyword(source, 'description: ')
-    corpus = [profile['description'] for profile in profiles]
+# [LaMP-2] Personalized Movie Tagging
+def _generate_query_corpus_classification_movies(
+    source: str,
+    profile: list[dict[str, str]]
+) -> tuple[str, list[str]]:
+    query = _extract_string_after(source, "description: ")
+    corpus = [rec["description"] for rec in profile]
     return query, corpus
 
 
-# =============================       LaMP 3: Personalized Product Rating       =============================
-def _generate_query_corpus_regression_review(source: str, profiles: list[Profile]) -> tuple[str, list[str]]:
-    query = _extract_string_after_keyword(source, 'review: ')
-    corpus = [profile['text'] for profile in profiles]
+# [LaMP-3] Personalized Product Rating
+def _generate_query_corpus_regression_review(
+    source: str,
+    profile: list[dict[str, str]]
+) -> tuple[str, list[str]]:
+    query = _extract_string_after(source, "review: ")
+    corpus = [rec["text"] for rec in profile]
     return query, corpus
 
 
-# ==============================  LaMP 4: Personalized News Headline Generation  =============================
-def _generate_query_corpus_generation_news(source: str, profiles: list[Profile]) -> tuple[str, list[str]]:
-    query = _extract_string_after_keyword(source, 'article: ')
-    corpus = [f'{profile["title"]} {profile["text"]}' for profile in profiles]
+# [LaMP-4] Personalized News Headline Generation
+def _generate_query_corpus_generation_news(
+    source: str,
+    profile: list[dict[str, str]]
+) -> tuple[str, list[str]]:
+    query = _extract_string_after(source, "article: ")
+    corpus = [f"{rec['title']} {rec['text']}" for rec in profile]
     return query, corpus
 
 
-# ============================= LaMP 5: Personalized Scholarly Title Generation =============================
-def _generate_query_corpus_generation_paper(source: str, profiles: list[Profile]) -> tuple[str, list[str]]:
-    query = _extract_string_after_keyword(source, 'paper: ')
-    corpus = [f'{profile["title"]} {profile["abstract"]}' for profile in profiles]
+# [LaMP-5] Personalized Scholarly Title Generation
+def _generate_query_corpus_generation_paper(
+    source: str,
+    profile: list[dict[str, str]]
+) -> tuple[str, list[str]]:
+    query = _extract_string_after(source, "paper: ")
+    corpus = [f"{rec['title']} {rec['abstract']}" for rec in profile]
     return query, corpus
 
 
-# =============================  LaMP 6: Personalized Email Subject Generation  =============================
-def _generate_query_corpus_generation_avocado(source: str, profiles: list[Profile]) -> tuple[str, list[str]]:
-    query = _extract_string_after_keyword(source, ': ')
-    corpus = [profile['text'] for profile in profiles]
+# [LaMP-7] Personalized Tweet Paraphrasing
+def _generate_query_corpus_generation_tweet(
+    source: str,
+    profile: list[dict[str, str]]
+) -> tuple[str, list[str]]:
+    query = _extract_string_after(source, ": ")
+    corpus = [rec["text"] for rec in profile]
     return query, corpus
 
 
-# =============================     LaMP 7: Personalized Tweet Paraphrasing     =============================
-def _generate_query_corpus_generation_tweet(source: str, profiles: list[Profile]) -> tuple[str, list[str]]:
-    query = _extract_string_after_keyword(source, ': ')
-    corpus = [profile['text'] for profile in profiles]
+# [LongLaMP-2] Personalized Abstract Generation
+def _generate_query_corpus_generation_abstract(
+    source: str,
+    profile: list[dict[str, str]]
+) -> tuple[str, list[str]]:
+    query = _extract_string_after(source, "items: ")
+    corpus = [f"{rec['title']} {rec['abstract']}" for rec in profile]
     return query, corpus
 
 
-# ============================     LongLaMP 1: Personalized Email Completion     ============================
-def _generate_query_corpus_generation_email(source: str, profiles: list[Profile]) -> tuple[str, list[str]]:
-    query = _extract_string_after_keyword(source, ': ')
-    corpus = [profile['text'] for profile in profiles]
-    return query, corpus
-
-
-# ===========================     LongLaMP 2: Personalized Abstract Generation     ===========================
-def _generate_query_corpus_generation_abstract(source: str, profiles: list[Profile]) -> tuple[str, list[str]]:
-    query = _extract_string_after_keyword(source, 'items: ')
-    corpus = [f'{profile["title"]} {profile["abstract"]}' for profile in profiles]
-    return query, corpus
-
-
-# ============================     LongLaMP 3: Personalized Topic Generation     =============================
-def _generate_query_corpus_generation_topic(source: str, profiles: list[Profile]) -> tuple[str, list[str]]:
-    corpus = [f'{profile["content"]} {profile["summary"]}' for profile in profiles]
-    return source, corpus
-
-
-# ========================     LongLaMP 4: Personalized Product Review Generation     ========================
-def _generate_query_corpus_generation_review(source: str, profiles: list[Profile]) -> tuple[str, list[str]]:
+# [LongLaMP-3] Personalized Product Review Generation
+def _generate_query_corpus_generation_review(
+    source: str,
+    profile: list[dict[str, str]]
+) -> tuple[str, list[str]]:
     corpus = [
-        f'{profile["overall"]} {profile["summary"]} {profile["description"]} {profile["reviewText"]}'
-        for profile in profiles
+        f"{rec['overall']} {rec['summary']} {rec['description']} {rec['reviewText']}"
+        for rec in profile
     ]
     return source, corpus
 
 
-# =============================                Utility Functions                =============================
+# [LongLaMP-4] Personalized Topic Generation
+def _generate_query_corpus_generation_topic(
+    source: str,
+    profile: list[dict[str, str]]
+) -> tuple[str, list[str]]:
+    corpus = [f"{rec['content']} {rec['summary']}" for rec in profile]
+    return source, corpus
+
+
+QUERY_CORPUS_GENERATORS = {
+    "LaMP-1": _generate_query_corpus_classification_citation,
+    "LaMP-2": _generate_query_corpus_classification_movies,
+    "LaMP-3": _generate_query_corpus_regression_review,
+    "LaMP-4": _generate_query_corpus_generation_news,
+    "LaMP-5": _generate_query_corpus_generation_paper,
+    "LaMP-7": _generate_query_corpus_generation_tweet,
+    "LongLaMP-2": _generate_query_corpus_generation_abstract,
+    "LongLaMP-3": _generate_query_corpus_generation_review,
+    "LongLaMP-4": _generate_query_corpus_generation_topic
+}
+
+
 def _extract_references(source: str) -> tuple[str, str]:
-    template_1 = 'Just answer with [1] or [2] without explanation. [1]: "'
-    template_2 = '" [2]: "'
+    delimiter_1 = 'Just answer with [1] or [2] without explanation. [1]: "'
+    delimiter_2 = '" [2]: "'
 
-    template_1_start = source.find(template_1)
-    template_2_start = source.find(template_2)
-    assert template_1_start != -1 and template_2_start != -1 and source.endswith('"')
+    index_1 = source.find(delimiter_1)
+    index_2 = source.find(delimiter_2)
+    assert index_1 != -1 and index_2 != -1 and source.endswith('"')
 
-    reference_1 = source[template_1_start + len(template_1) : template_2_start]
-    reference_2 = source[template_2_start + len(template_2) : -1]
+    reference_1 = source[index_1 + len(delimiter_1) : index_2]
+    reference_2 = source[index_2 + len(delimiter_2) : -1]
     return reference_1, reference_2
 
 
-def _extract_string_after_keyword(source: str, keyword: str) -> str:
-    keyword_start = source.find(keyword)
-
-    if keyword_start == -1:
-        raise ValueError(f'Keyword "{keyword}" not found in input')
-
-    return source[keyword_start + len(keyword):]
+def _extract_string_after(source: str, delimiter: str) -> str:
+    index = source.find(delimiter)
+    assert index != -1
+    return source[index + len(delimiter):]
