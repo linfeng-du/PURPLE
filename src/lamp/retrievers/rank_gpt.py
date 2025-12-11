05157fac83ecc1610cf6a64800f8515d611cd1ce
linfeng-du <linfeng9918@gmail.com>
2025-12-11 02:11:17 -0500

    update rank_gpt

--- src/lamp/retrievers/rank_gpt.py
+++ src/lamp/retrievers/rank_gpt.py
@@ -1,136 +1,160 @@
-# Adapted from https://github.com/sunnweiwei/RankGPT/blob/main/rank_gpt.py
+# Adapted from: https://github.com/sunnweiwei/RankGPT/blob/main/rank_gpt.py
 import logging
 import time
-from typing import TypeAlias
 
-from transformers import pipeline
 from openai import OpenAI, OpenAIError
-
-from ..data_types import Profile
+from transformers import pipeline
 
 
 logger = logging.getLogger(__name__)
-Message: TypeAlias = list[dict[str, str]]
 
 
 class RankGPT:
 
-    def __init__(self, model: str) -> None:
+    def __init__(
+        self,
+        model: str,
+        backend: str,
+        window_size: int = 20,
+        window_stride: int = 10,
+        max_passage_length: int = 300
+    ) -> None:
         self.model = model
+        self.backend = backend
 
-        if self.model == 'llama3':
+        self.window_size = window_size
+        self.window_stride = window_stride
+        self.max_passage_length = max_passage_length
+
+        if self.backend == "hf":
             self.pipeline = pipeline(
-                task='text-generation',
-                model='meta-llama/Meta-Llama-3-8B-Instruct',
-                device='cuda',
-                torch_dtype='bfloat16'
+                task="text-generation",
+                model=self.model,
+                device_map="auto",
+                torch_dtype="bfloat16"
             )
-            self.pipeline.tokenizer.padding_side = 'left'
-
-            if self.pipeline.tokenizer.pad_token is None:
-                self.pipeline.tokenizer.pad_token = self.pipeline.tokenizer.eos_token
-                self.pipeline.model.generation_config.pad_token_id = self.pipeline.tokenizer.eos_token_id
-        elif self.model == 'gpt5':
-            self.client = OpenAI(base_url='https://api.openai.com/v1')
+        elif self.backend == "openai":
+            self.client = OpenAI()
         else:
-            raise ValueError(f'Invalid model for RankGPT: {self.model}')
+            raise ValueError(f"Invalid backend: {self.backend}")
 
     def __call__(
-        self, query: str, corpus: list[str], profiles: list[Profile], num_rerank: int,
-        window_size: int = 20, step: int = 10
-    ) -> list[Profile]:
-        rank_start = len(profiles) - window_size
-        rank_end = len(profiles)
-        indices = list(range(len(profiles)))
+        self,
+        query: str,
+        corpus: list[str],
+        profile: list[dict[str, str]],
+        num_retrieve: int
+    ) -> list[dict[str, str]]:
+        assert len(corpus) == len(profile) != 0
+        num_retrieve = min(num_retrieve, len(profile))
 
-        while rank_start >= 0:
-            message = _create_ranking_instruction(query, corpus, rank_start, rank_end)
+        corpus = corpus.copy()
+        ranking = list(range(len(profile)))
+        end = len(profile)
 
-            if self.model == 'llama3':
+        while end > 0:
+            start = max(0, end - self.window_size)
+            messages = _build_instruction(query, corpus[start:end], self.max_passage_length)
+
+            if self.backend == "hf":
                 outputs = self.pipeline(
-                    message,
+                    messages,
                     max_new_tokens=256,
                     do_sample=False,
                     temperature=None,
-                    top_p=None
+                    top_p=None,
+                    pad_token_id=self.pipeline.tokenizer.eos_token_id
                 )
-                response = outputs[0]['generated_text'][-1]['content']
-            elif self.model == 'gpt5':
+                response = outputs[0]["generated_text"][-1]["content"]
+            elif self.backend == "openai":
                 response = None
                 num_retries = 0
 
                 while response is None:
                     try:
-                        outputs = self.client.chat.completions.create(messages=message, model='gpt-5-nano')
+                        outputs = self.client.chat.completions.create(
+                            messages=messages,
+                            model=self.model
+                        )
                         response = outputs.choices[0].message.content
                     except OpenAIError as err:
-                        logger.error(f'OpenAI API error: {err}', exc_info=True)
                         num_retries += 1
-                        time.sleep(min(2 ** num_retries, 60))
 
-            indices = _receive_ranking(indices, response, rank_start, rank_end)
-            corpus = [corpus[index] for index in indices]
-            profiles = [profiles[index] for index in indices]
+                        if num_retries > 10:
+                            raise err
 
-            rank_start -= step
-            rank_end -= step
+                        logger.error(f"OpenAI API error: {err}", exc_info=True)
+                        time.sleep(num_retries ** 2)
 
-        return profiles[:num_rerank]
+            ordering = _parse_response(response, end - start)
+            corpus[start:end] = [corpus[start:end][idx] for idx in ordering]
+            ranking[start:end] = [ranking[start:end][idx] for idx in ordering]
 
+            end -= self.window_stride
 
-def _create_ranking_instruction(
-    query: str, corpus: list[str],
-    rank_start: int, rank_end: int,
-    max_length: int = 300
-) -> Message:
-    num_passages = len(corpus[rank_start:rank_end])
-    message = _create_prefix_prompt(query, num_passages)
-
-    for index, document in enumerate(corpus[rank_start:rank_end], start=1):
-        content = ' '.join(document.strip().split()[:max_length])
-        message.append({'role': 'user', 'content': f'[{index}] {content}'})
-        message.append({'role': 'assistant', 'content': f'Received passage [{index}].'})
-
-    message.append({'role': 'user', 'content': _create_postfix_prompt(query, num_passages)})
-    return message
+        retrieved_profile = [profile[idx] for idx in ranking[:num_retrieve]]
+        return retrieved_profile
 
 
-def _create_prefix_prompt(query: str, num_passages: int) -> Message:
+def _build_instruction(
+    query: str,
+    corpus: list[str],
+    max_passage_length: int
+) -> list[dict[str, str]]:
+    messages = _build_prefix(query, len(corpus))
+
+    for index, passage in enumerate(corpus, start=1):
+        passage = " ".join(passage.strip().split()[:max_passage_length])
+        messages.extend([
+            {"role": "user", "content": f"[{index}] {passage}"},
+            {"role": "assistant", "content": f"Received passage [{index}]."}
+        ])
+
+    messages.append(_build_suffix(query, len(corpus)))
+    return messages
+
+
+def _build_prefix(query: str, num_passages: int) -> list[dict[str, str]]:
     return [
-        {'role': 'system', 'content': (
-            'You are RankGPT, an intelligent assistant that can '
-            'rank passages based on their relevancy to the query.'
-        )},
-        {'role': 'user', 'content': (
-            f'I will provide you with {num_passages} passages, each indicated by number identifier []. \n'
-            f'Rank the passages based on their relevance to query: {query}.'
-        )},
-        {'role': 'assistant', 'content': 'Okay, please provide the passages.'}
+        {
+            "role": "system",
+            "content": (
+                "You are RankGPT, an intelligent assistant "
+                "that can rank passages based on their relevancy to the query."
+            )
+        },
+        {
+            "role": "user",
+            "content": (
+                f"I will provide you with {num_passages} passages, "
+                "each indicated by number identifier []. \n"
+                f"Rank the passages based on their relevance to query: {query}."
+            )
+        },
+        {"role": "assistant", "content": "Okay, please provide the passages."}
     ]
 
 
-def _create_postfix_prompt(query: str, num_passages: int) -> str:
-    return (
-        f'Search Query: {query}. \n'
-        f'Rank the {num_passages} passages above based on their relevance to the search query. '
-        'The passages should be listed in descending order using identifiers. '
-        'The most relevant passages should be listed first. '
-        'The output format should be [] > [], e.g., [1] > [2]. '
-        'Only response the ranking results, do not say any word or explain.'
-    )
+def _build_suffix(query: str, num_passages: int) -> dict[str, str]:
+    return {
+        "role": "user",
+        "content": (
+            f"Search Query: {query}. \n"
+            f"Rank the {num_passages} passages above "
+            "based on their relevance to the search query. "
+            "The passages should be listed in descending order using identifiers. "
+            "The most relevant passages should be listed first. "
+            "The output format should be [] > [], e.g., [1] > [2]. "
+            "Only response the ranking results, do not say any word or explain."
+        )
+    }
 
 
-def _receive_ranking(indices: list[int], response: str, rank_start: int, rank_end: int) -> list[int]:
-    ranking = ''.join((char if char.isdigit() else ' ') for char in response)
-    ranking = [int(char) - 1 for char in ranking.strip().split()]
-    ranking = list(dict.fromkeys(ranking))
+def _parse_response(response: str, num_passages: int) -> list[int]:
+    digits_str = "".join(char if char.isdigit() else " " for char in response)
+    digits = [int(char) - 1 for char in digits_str.strip().split()]
+    unique_digits = list(dict.fromkeys(digits))
 
-    window_indices = indices[rank_start:rank_end].copy()
-    original_ranking = list(range(len(window_indices)))
-    ranking = [rank for rank in ranking if rank in original_ranking]
-    ranking += [rank for rank in original_ranking if rank not in ranking]
-
-    for index, rank in enumerate(ranking):
-        indices[index + rank_start] = window_indices[rank]
-
-    return indices
+    ordering = [idx for idx in unique_digits if idx in range(num_passages)]
+    ordering += [idx for idx in range(num_passages) if idx not in unique_digits]
+    return ordering
