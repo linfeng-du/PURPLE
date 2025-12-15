e6fd385c09906a684c02757ac20d2d361a0e9ef9
linfeng-du <linfeng9918@gmail.com>
2025-12-14 22:53:29 -0500

    update __init__

--- src/lamp/__init__.py
+++ src/lamp/__init__.py
@@ -1,4 +1,4 @@
 from .dataset import load_lamp_dataset
 from .metric import create_metric_fn
 from .prompt import create_prompt_fn
-from .retrievers import get_retriever_fn
+from .retrievers import create_retriever_fn
