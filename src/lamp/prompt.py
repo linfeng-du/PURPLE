bead83322863c41aa916e627b94bd7335cf12b24
linfeng-du <linfeng9918@gmail.com>
2025-12-15 15:23:38 -0500

    update prompt

--- src/lamp/prompt.py
+++ src/lamp/prompt.py
@@ -36,11 +36,7 @@ def create_prompt_fn(
     ) -> str:
         local_factor = factor
         retrieved_profile = retriever_fn(query, corpus, profile, num_retrieve)
-        source_length = len(
-            tokenizer.encode(
-                source, truncation=True, max_length=max_prompt_length
-            )
-        )
+        source_length = len(tokenizer.encode(source))
 
         while True:
             try:
