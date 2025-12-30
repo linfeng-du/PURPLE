ac061b4db6ca28819f7421ea1264360269aea320
linfeng-du <linfeng9918@gmail.com>
2025-12-30 17:37:50 -0500

    update grpo

---
+++ src/purple/grpo.py
@@ -0,0 +1,163 @@
+from typing import TypedDict
+
+import torch
+from torch.utils.data import Dataset
+from transformers import BatchEncoding
+
+
+class RolloutGroup(TypedDict):
+    query_inputs: dict[str, torch.Tensor]
+    corpus_inputs: list[BatchEncoding]
+    record_mask: torch.Tensor
+    rollout_indices: torch.Tensor
+    rollout_logps: torch.Tensor
+    advantages: torch.Tensor
+
+
+class RolloutGroupBatch(TypedDict):
+    query_inputs: BatchEncoding
+    corpus_inputs: list[list[BatchEncoding]]
+    record_mask: torch.Tensor
+    rollout_indices: torch.Tensor
+    rollout_logps: torch.Tensor
+    advantages: torch.Tensor
+
+
+class RolloutDataset(Dataset):
+    def __init__(
+        self,
+        query_inputs: BatchEncoding,
+        corpus_inputs: list[list[BatchEncoding]],
+        record_mask: torch.Tensor,
+        rollout_indices: torch.Tensor,
+        rollout_logps: torch.Tensor,
+        advantages: torch.Tensor
+    ) -> None:
+        self.examples = []
+
+        for index in range(len(corpus_inputs)):
+            self.examples.append(
+                RolloutGroup(
+                    query_inputs={
+                        k: v[index] for k, v in query_inputs.items()
+                    },
+                    corpus_inputs=corpus_inputs[index],
+                    record_mask=record_mask[index],
+                    rollout_indices=rollout_indices[index],
+                    rollout_logps=rollout_logps[index],
+                    advantages=advantages[index]
+                )
+            )
+
+    def __len__(self) -> int:
+        return len(self.examples)
+
+    def __getitem__(self, index: int) -> RolloutGroup:
+        return self.examples[index]
+
+
+def sample_rollouts(
+    likelihoods: torch.Tensor,
+    num_rollouts: int,
+    num_retrieve: int
+) -> torch.Tensor:
+    max_num_retrieve = (likelihoods > 0).sum(dim=-1).min().item()
+    num_retrieve = min(num_retrieve, max_num_retrieve)
+
+    rollout_indices = []
+    rollout_logps = []
+
+    for _ in range(num_rollouts):
+        indices = torch.full_like(
+            likelihoods[:, :num_retrieve], fill_value=-1, dtype=torch.long
+        )
+        logps = torch.zeros_like(likelihoods[:, :num_retrieve])
+
+        likelihoods_ = likelihoods
+
+        for index in range(num_retrieve):
+            probs = likelihoods_ / likelihoods_.sum(dim=-1, keepdim=True)
+            chosen_indices = torch.multinomial(probs, num_samples=1)
+            chosen_probs = probs.gather(dim=-1, index=chosen_indices)
+
+            indices[:, index] = chosen_indices.squeeze(dim=-1)
+            logps[:, index] = chosen_probs.log().squeeze(dim=-1)
+
+            likelihoods_ = likelihoods_.scatter(
+                dim=-1, index=chosen_indices, value=0.0
+            )
+
+        rollout_indices.append(indices)
+        rollout_logps.append(logps.sum(dim=-1))
+
+    rollout_indices = torch.stack(rollout_indices, dim=1)
+    rollout_logps = torch.stack(rollout_logps, dim=1)
+    return rollout_indices, rollout_logps
+
+
+def compute_rollout_logps(
+    likelihoods: torch.Tensor,
+    rollout_indices: torch.Tensor
+) -> torch.Tensor:
+    _, num_rollouts, num_retrieve = rollout_indices.shape
+
+    rollout_logps = torch.zeros_like(
+        rollout_indices[:, :, 0], dtype=torch.float32
+    )
+    likelihoods = likelihoods.unsqueeze(dim=1).expand(-1, num_rollouts, -1)
+
+    for index in range(num_retrieve):
+        chosen_indices = rollout_indices[:, :, index].unsqueeze(dim=-1)
+        chosen_likelihoods = (
+            likelihoods.gather(dim=-1, index=chosen_indices).squeeze(dim=-1)
+        )
+
+        rollout_logps += (
+            chosen_likelihoods.log() - likelihoods.sum(dim=-1).log()
+        )
+        likelihoods = likelihoods.scatter(
+            dim=-1, index=chosen_indices, value=0.0
+        )
+
+    return rollout_logps
+
+
+def collate_fn(examples: list[RolloutGroup]) -> RolloutGroupBatch:
+    query_inputs = {}
+
+    for key in examples[0]["query_inputs"].keys():
+        query_inputs[key] = torch.stack(
+            [e["query_inputs"][key] for e in examples]
+        )
+
+    query_inputs = BatchEncoding(query_inputs)
+    corpus_inputs = [e["corpus_inputs"] for e in examples]
+    record_mask = torch.stack([e["record_mask"] for e in examples])
+    rollout_indices = torch.stack([e["rollout_indices"] for e in examples])
+    rollout_logps = torch.stack([e["rollout_logps"] for e in examples])
+    advantages = torch.stack([e["advantages"] for e in examples])
+
+    return RolloutGroupBatch(
+        query_inputs=query_inputs,
+        corpus_inputs=corpus_inputs,
+        record_mask=record_mask,
+        rollout_indices=rollout_indices,
+        rollout_logps=rollout_logps,
+        advantages=advantages
+    )
+
+
+def compute_loss(
+    rollout_logps: torch.Tensor,
+    rollout_batch: RolloutGroupBatch,
+    epsilon: float
+) -> torch.Tensor:
+    old_rollout_logps = rollout_batch["rollout_logps"]
+    advantages = rollout_batch["advantages"]
+
+    ratio = (rollout_logps - old_rollout_logps).exp()
+    surrogate_1 = ratio * advantages
+    surrogate_2 = ratio.clamp(min=1 - epsilon, max=1 + epsilon) * advantages
+
+    loss = -torch.min(surrogate_1, surrogate_2)
+    return loss.mean()
